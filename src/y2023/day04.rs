3410d3784affe1fc9e36d178ad3f5c746a36c8c7
Laszlo Lenkei <laszlo.lenkei.ext@siemens-healthineers.com>
2023-12-04 22:29:30 +0100

    day 4 cleaned

--- src/y2023/day04.rs
+++ src/y2023/day04.rs
@@ -3,23 +3,9 @@ use std::collections::HashMap;
 
 pub fn part1() {
     let sum: usize = iter_lines_from("res/2023/input04.txt")
-        .map(|line| {
-            let (winners, yours) = line.split_once(": ").unwrap().1.split_once(" | ").unwrap();
-            let mut w = [false; 100];
-            for n in winners.split(' ').filter(|n| !n.is_empty()) {
-                w[n.parse::<usize>().unwrap()] = true;
-            }
-            let mut exp: i32 = -1;
-            for n in yours.split(' ').filter(|n| !n.is_empty()) {
-                if w[n.parse::<usize>().unwrap()] {
-                    exp += 1;
-                }
-            }
-            if exp >= 0 {
-                2_usize.pow(exp as u32)
-            } else {
-                0
-            }
+        .map(|line| match count_wins(line) {
+            0 => 0,
+            n => 2_usize.pow((n - 1) as u32),
         })
         .sum();
     println!("{sum}");
@@ -29,25 +15,27 @@ pub fn part2() {
     let mut copies: HashMap<usize, usize> = HashMap::new();
     for (card_idx, line) in iter_lines_from("res/2023/input04.txt").enumerate() {
         copies.entry(card_idx).and_modify(|c| *c += 1).or_insert(1);
-        let (winners, yours) = line.split_once(": ").unwrap().1.split_once(" | ").unwrap();
-        let mut w = [false; 100];
-        for n in winners.split(' ').filter(|n| !n.is_empty()) {
-            w[n.parse::<usize>().unwrap()] = true;
-        }
-        let mut wins: usize = 0;
-        for n in yours.split(' ').filter(|n| !n.is_empty()) {
-            if w[n.parse::<usize>().unwrap()] {
-                wins += 1;
-            }
-        }
         let current_copies = *copies.get(&card_idx).unwrap();
-        for win in 1..=wins {
+        for win_id in 1..=count_wins(line) {
             copies
-                .entry(card_idx + win)
+                .entry(card_idx + win_id)
                 .and_modify(|c| *c += current_copies)
                 .or_insert(current_copies);
         }
     }
     let sum: usize = copies.values().sum();
     println!("{sum}");
+}
+
+fn count_wins(line: String) -> usize {
+    let (winners, yours) = line.split_once(": ").unwrap().1.split_once(" | ").unwrap();
+    let mut winner_table = [false; 100];
+    for n in winners.split(' ').filter_map(|n| n.parse::<usize>().ok()) {
+        winner_table[n] = true;
+    }
+    yours
+        .split(' ')
+        .filter_map(|n| n.parse::<usize>().ok())
+        .filter(|&n| winner_table[n])
+        .count()
 }
