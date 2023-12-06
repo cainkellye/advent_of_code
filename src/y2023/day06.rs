90544a5d229fa446ab4d84cbb55f038df3c9efe3
CainKellye <cain.kellye@gmail.com>
2023-12-06 18:16:40 +0100

    cleanup day 6

--- src/y2023/day06.rs
+++ src/y2023/day06.rs
@@ -1,57 +1,51 @@
 use super::*;
 use roots::{find_roots_quadratic, Roots::Two};
 
+/// Reasoning
+/// --------------
+/// t = time limit; p = push time; d = distance
+/// d = (t-p) * p
+/// Boat wins if p satisfies:
+/// 0 < tp - pp - d
+/// pp - tp + d < 0
 pub fn part1() {
-    let races = {
-        let mut lines = iter_lines_from("res/2023/input06.txt");
-
-        let first_line = lines.next().unwrap();
-        let (_, times) = first_line.split_once(":").unwrap();
-        let times = times
-            .split_whitespace()
-            .filter_map(|t| t.parse::<usize>().ok());
-
-        let second_line = lines.next().unwrap();
-        let (_, distances) = second_line.split_once(":").unwrap();
-        let distances = distances
-            .split_whitespace()
-            .filter_map(|t| t.parse::<usize>().ok());
-
-        times.zip(distances).collect_vec()
-    };
-    let solution: usize = races
-        .into_iter()
-        .map(|(time, distance)| {
-            let Two([low, high]) =
-                find_roots_quadratic(1_f32, -1_f32 * time as f32, distance as f32)
-            else {
-                unreachable!()
-            };
-            (high.ceil() - low.ceil()) as usize
+    let (times, distances) = iter_lines_from("res/2023/input06.txt")
+        .filter_map(|line| line.split_once(":").map(|(_, x)| x.to_owned()))
+        .map(|numbers| {
+            numbers
+                .split_whitespace()
+                .filter_map(|n| n.parse::<f64>().ok())
+                .collect_vec()
         })
-        .product();
+        .collect_tuple()
+        .unwrap();
+
+    let solution: usize = times.into_iter().zip(distances.into_iter())
+        .map(|(time, distance)| {
+            if let Two([low, high]) =
+                find_roots_quadratic(1_f64, -(time as f64), distance as f64)
+            {
+                (high.ceil() - low.ceil()) as usize
+            } else {
+                1
+            }
+        }).product();
     println!("{solution}");
 }
 
 pub fn part2() {
-    let (time, distance) = dbg!({
-        let mut lines = iter_lines_from("res/2023/input06.txt");
+    let (time, distance) = iter_lines_from("res/2023/input06.txt")
+        .filter_map(|line| line.split_once(":").map(|(_, x)| x.to_owned()))
+        .filter_map(|number| number.replace(" ", "").parse::<usize>().ok())
+        .collect_tuple()
+        .unwrap();
 
-        let first_line = lines.next().unwrap();
-        let (_, time) = first_line.split_once(":").unwrap();
-        let time = time.replace(" ", "").parse::<usize>().unwrap();
-
-        let second_line = lines.next().unwrap();
-        let (_, distance) = second_line.split_once(":").unwrap();
-        let distance = distance.replace(" ", "").parse::<usize>().unwrap();
-        (time, distance)
-    });
-    let solution: usize = {
-        let Two([low, high]) = find_roots_quadratic(1_f64, -1_f64 * time as f64, distance as f64)
-        else {
-            unreachable!()
-        };
+    let solution: usize = if let Two([low, high]) =
+        find_roots_quadratic(1_f64, -(time as f64), distance as f64)
+    {
         (high.ceil() - low.ceil()) as usize
+    } else {
+        1
     };
     println!("{solution}");
 }
