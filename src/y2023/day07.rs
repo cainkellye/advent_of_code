f426de33b87827ee329df40ae610a79bf72970f1
Laszlo Lenkei <laszlo.lenkei.ext@siemens-healthineers.com>
2023-12-07 14:52:19 +0100

    day 7 done

--- src/y2023/day07.rs
+++ src/y2023/day07.rs
@@ -1,28 +1,69 @@
 use super::*;
+use std::cmp::Ordering;
+
 pub fn part1() {
-    part1_internal("res/2023/input07.txt")
+    println!("{}", solve(Part::One)); //250602641
 }
+
 pub fn part2() {
-    part2_internal("res/2023/input07.txt")
+    println!("{}", solve(Part::Two)); //251037509
 }
 
-fn part1_internal(input_file: &str) {}
+#[derive(PartialEq, Eq, Clone, Copy)]
+enum Part {
+    One,
+    Two,
+}
 
-fn part2_internal(input_file: &str) {}
+fn solve(p: Part) -> usize {
+    iter_lines_from("res/2023/input07.txt")
+        .filter_map(|line| {
+            line.split_once(" ")
+                .map(|(a, b)| (a.to_owned(), b.to_owned()))
+        })
+        .sorted_by(|(a, _), (b, _)| compare_hands(a, b, p))
+        .enumerate()
+        .map(|(idx, (_, bid))| (idx + 1) * bid.parse::<usize>().unwrap())
+        .sum()
+}
 
-fn parse_input(input_file: &str) {}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn test_part1() {
-        assert_eq!(part1_internal("res/2023/test07.txt"), ());
+fn compare_hands(a: &String, b: &String, p: Part) -> Ordering {
+    let cmp = score_hand(a, p).cmp(&score_hand(b, p));
+    if cmp != Ordering::Equal {
+        return cmp;
     }
+    let (a, b) = a
+        .chars()
+        .zip(b.chars())
+        .find(|(a, b)| a != b)
+        .expect("totally same hands");
+    face_value(a, p).cmp(&face_value(b, p))
+}
 
-    #[test]
-    fn test_part2() {
-        assert_eq!(part2_internal("res/2023/test07.txt"), ());
+fn score_hand(hand: &String, p: Part) -> [u8; 2] {
+    let mut faces = [0_u8; 13];
+    let mut jokers = 0;
+    for c in hand.chars() {
+        if p == Part::Two && c == 'J' {
+            jokers += 1;
+        } else {
+            faces[face_value(c, p)] += 1;
+        }
     }
+    faces.sort_unstable();
+    let mut score: [u8; 2] = faces[11..].try_into().unwrap();
+    score.reverse();
+    score[0] += jokers;
+    score
+}
+
+fn face_value(c: char, p: Part) -> usize {
+    if p == Part::One {
+        b"23456789TJQKA"
+    } else {
+        b"J23456789TQKA"
+    }
+    .iter()
+    .position(|&face| face == c as u8)
+    .unwrap()
 }
