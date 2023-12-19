0e354044b0763c7986f8e955ae27824919a894bf
CainKellye <cain.kellye@gmail.com>
2023-12-19 08:16:48 +0100

    simplified

--- src/y2023/day18.rs
+++ src/y2023/day18.rs
@@ -1,6 +1,5 @@
 use super::*;
-use geo::Area;
-use geo::Polygon;
+use geo::{Area, EuclideanLength, LineString, Polygon};
 
 pub fn part1() {
     println!("{:?}", part1_internal("res/2023/input18.txt")); // 47675
@@ -12,7 +11,6 @@ pub fn part2() {
 fn part1_internal(input_file: &str) -> usize {
     let mut digged = vec![(0., 0.)];
     let mut position = (0., 0.);
-    let mut trench = 0;
     for (direction, count, _) in parse_input(input_file) {
         match direction {
             b'U' => position.0 -= count as f64,
@@ -21,38 +19,30 @@ fn part1_internal(input_file: &str) -> usize {
             b'R' => position.1 += count as f64,
             _ => unreachable!(),
         }
-        trench += count as usize;
         digged.push(position);
     }
-    let poly = Polygon::new(digged.into(), vec![]);
-    poly.unsigned_area() as usize + trench / 2 + 1
+    let line_string = LineString::from(digged);
+    let trench = line_string.euclidean_length() as usize;
+    Polygon::new(line_string, vec![]).unsigned_area() as usize + trench / 2 + 1
 }
 
 fn part2_internal(input_file: &str) -> usize {
     let mut digged = vec![(0., 0.)];
     let mut position = (0., 0.);
-    let mut trench = 0;
     for (_, _, color_hex) in parse_input(input_file) {
         let count = usize::from_str_radix(&color_hex[1..6], 16).unwrap();
-        let direction = match &color_hex[6..7] {
-            "0" => b'R',
-            "1" => b'D',
-            "2" => b'L',
-            "3" => b'U',
+        match &color_hex[6..7] {
+            "0" => position.1 += count as f64,
+            "1" => position.0 += count as f64,
+            "2" => position.1 -= count as f64,
+            "3" => position.0 -= count as f64,
             _ => unreachable!(),
         };
-        match direction {
-            b'U' => position.0 -= count as f64,
-            b'D' => position.0 += count as f64,
-            b'L' => position.1 -= count as f64,
-            b'R' => position.1 += count as f64,
-            _ => unreachable!(),
-        }
-        trench += count;
         digged.push(position);
     }
-    let poly = Polygon::new(digged.into(), vec![]);
-    poly.unsigned_area() as usize + trench / 2 + 1
+    let line_string = LineString::from(digged);
+    let trench = line_string.euclidean_length() as usize;
+    Polygon::new(line_string, vec![]).unsigned_area() as usize + trench / 2 + 1
 }
 
 fn parse_input(input_file: &str) -> impl Iterator<Item = (u8, u8, String)> {
