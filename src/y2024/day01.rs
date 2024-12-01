7641121ea86dc27515a633202a5c18c38893ba41
CainKellye <cain.kellye@gmail.com>
2024-12-01 15:49:26 +0100

    Optimize day01

--- src/y2024/day01.rs
+++ src/y2024/day01.rs
@@ -1,9 +1,11 @@
 use super::*;
+use std::collections::HashMap;
+
 pub fn part1() {
     //distances
     let (mut list1, mut list2) = read_lists();
-    list1.sort();
-    list2.sort();
+    list1.sort_unstable();
+    list2.sort_unstable();
     let sum: u32 = list1
         .into_iter()
         .zip(list2)
@@ -13,24 +15,32 @@ pub fn part1() {
 }
 
 pub fn part2() {
+    //frequencies
     let (list1, list2) = read_lists();
-    let sum: usize = list1
-        .into_iter()
-        .map(|n| list2.iter().filter(|&&k| k == n).count() * n as usize)
+    let mut freq_map = HashMap::new();
+
+    for &num in &list2 {
+        *freq_map.entry(num).or_insert(0) += 1;
+    }
+
+    let sum: u32 = list1
+        .iter()
+        .filter_map(|&n| freq_map.get(&n).map(|&count| count * n))
         .sum();
+
     println!("{:?}", sum);
 }
 
 fn read_lists() -> (Vec<u32>, Vec<u32>) {
-    let (list1, list2) = iter_lines_from("res/2024/input01.txt")
-        .map(|l| {
-            let (a, b) = l.split_once("   ").unwrap();
-            (a.parse::<u32>().unwrap(), b.parse::<u32>().unwrap())
-        })
-        .fold((Vec::new(), Vec::new()), |(mut vec1, mut vec2), (a, b)| {
-            vec1.push(a);
-            vec2.push(b);
-            (vec1, vec2)
-        });
+    let mut list1 = Vec::new();
+    let mut list2 = Vec::new();
+
+    for line in iter_lines_from("res/2024/input01.txt") {
+        if let Some((a, b)) = line.split_once("   ") {
+            list1.push(a.parse::<u32>().unwrap());
+            list2.push(b.parse::<u32>().unwrap());
+        }
+    }
+
     (list1, list2)
 }
